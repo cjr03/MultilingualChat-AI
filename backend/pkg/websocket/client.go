ce583976f3a3e7add89ecb288b74267cf6e17a7c
cjr03 <161370035+cjr03@users.noreply.github.com>
2024-03-08 16:59:24 -0800

    Update client.go

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+import (
+	"context"
+	"os"
+	gpt3 "github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/viper"
+)
+
+var langauge_translator string = "The following message should be translated into " + language + ". If it is already in " + language + " please output only the message. If it must be translated, please output only the translated message as your answer. The message is: "
+var language string = "English"
+
 type Client struct {
 	ID   string
 	Conn *websocket.Conn
@@ -17,6 +27,22 @@ type Client struct {
 type Message struct {
 	Type int    `json:"type"`
 	Body string `json:"body"`
+}
+
+func GetResponse(c *Client, client gpt3.Client, ctx context.Context, quesiton string) {
+	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			quesiton,
+		},
+		MaxTokens:   gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	}, func(resp *gpt3.CompletionResponse) {
+		c.Pool.Broadcast <- Message{Type: 1, Body: resp.Choices[0].Text}
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
 }
 
 func (c *Client) Read() {
@@ -31,10 +57,18 @@ func (c *Client) Read() {
 			log.Println(err)
 			return
 		}
-
 		message := Message{Type: messageType, Body: string(p)}
-		c.Pool.Broadcast <- message
+		viper.SetConfigFile(".env")
+		viper.ReadInConfig()
+		//apiKey := viper.GetString("API_KEY")
+		apiKey := os.Getenv("API_KEY")
+		fmt.Println("API Key:", apiKey)
+		if apiKey == "" {
+			panic("Missing API KEY")
+		}
+		ctx := context.Background()
+		client := gpt3.NewClient(apiKey)
+		GetResponse(c, client, ctx, langauge_translator + message.Body)
 		fmt.Printf("Message Received: %+v\n", message)
-
 	}
 }
